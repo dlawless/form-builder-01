768f7663aad3f44b0e94809f3ba607d41192bff9
dlawless <derek@dereklawless.com>
2014-12-25 01:24:21 -0500

    New 2

--- server.js
+++ server.js
@@ -1,15 +1,49 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
 var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if(app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+};
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 var port = process.env.PORT || 8080;
-
-app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-    res.render('index');
-});
 
 app.listen(port, function(){
     console.log('Our app is running on http://localhost:' + port);
