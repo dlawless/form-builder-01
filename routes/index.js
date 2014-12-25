83fc4edd119459858c14b824acf93ff2160220e7
dlawless <derek@dereklawless.com>
2014-12-25 01:23:18 -0500

    New

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,13 @@
-/**
- * Created by Derek on 12/25/2014.
- */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Form Builder' });
+});
+
+router.get('/test', function(req, res) {
+    res.render('test');
+});
+
+module.exports = router;
