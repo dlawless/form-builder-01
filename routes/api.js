10e1cae874dac32d58534c9815edc21784d9c825
Derek Lawless <derek@dereklawless.com>
2015-01-01 20:07:07 -0500

    Working with ObjectID

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoskin = require('mongoskin');
+var ObjectID = require('mongoskin').ObjectID;
 
 var db = mongoskin.db('mongodb://form_builder_user:password1@ds063140.mongolab.com:63140/heroku_app32834158', {safe:true});
 
@@ -17,7 +18,7 @@ router.get('/forms', function(req, res) {
 });
 
 router.get('/forms/:id', function(req, res) {
-    db.collection('forms').findById({_id:req.params.id}, function(err, result) {
+    db.collection('forms').findById({_id:new ObjectID(req.params.id)}, function(err, result) {
        res.send(result);
     });
 });
