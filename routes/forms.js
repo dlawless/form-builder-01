b4c57ffb94091bd244154b115e031d87f2f01d8d
Derek Lawless <derek@dereklawless.com>
2015-01-01 20:14:06 -0500

    Added forms page

---
+++ routes/forms.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var mongoskin = require('mongoskin');
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('forms', { title: 'Form Builder' });
+});
+
+module.exports = router;
